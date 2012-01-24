8ad027751a8a467b1d4c1eb7a3cbfbb508616179
B Kleinen <kleinen@infrastructure.de>
2012-01-24 10:32:28 +0100

    generated app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Icards::Application.config.secret_token = '632c7a36abc5af2b46ad18f97676dd8e35715bb1c965c87f28c739ed1f057cb62fa54b52fa5ff9c11758ab1142312d617c1255ee2d0bd8ae6d758f42c615b929'
